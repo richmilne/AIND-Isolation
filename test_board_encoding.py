d00c3522a982afd778e058a4e5aa6a49a853f6ee
Richard Milne <richmilne@hotmail.com>
2017-03-28 17:09:16 +0200

    Rewrote tests to use an encoding class, which now take the size of the board as a parameter (no longer hard-coded)

--- test_board_encoding.py
+++ test_board_encoding.py
@@ -1,70 +1,11 @@
 import sys
-# Hard-coded for 5 * 5 board
-# Each co-ord can be represented by tup of 2 3-bit numbers
-# Bit pattern 0 reserved for a player co-ord of None
-# Otherwise, board is just a bitmap, with 1 if a cell is occupied.
-
-# Translate co-ords so that board rotations can be done about the centre
-def create_xlate_fns(offset):
-    # xlate all board co-ords up and to left
-    up_left = lambda x:None if x is None else (x[0]-offset, x[1]-offset)
-    # shift the co-ords back
-    down_rt = lambda x:None if x is None else (x[0]+offset, x[1]+offset)
-    return up_left, down_rt
-
-up_left, down_rt = create_xlate_fns(5//2)
-
-def coord_to_num(coord):
-    return coord[0] * 5 + coord[1]
-
-def num_to_coord(num):
-    return (num // 5, num % 5)
-
-
-def format_bitmap(bitmap):
-    bit_str = bin(bitmap)[2:].zfill(35)
-    return ' '.join((bit_str[:5], bit_str[5:10], bit_str[10:]))
-
-def encode_player(coord):
-    if coord is None:
-        return 0
-    return coord_to_num(coord) + 1
-
-def decode_player(number):
-    player = number & 0b11111
-    if player == 0:
-        return None
-    return num_to_coord(player-1)
-
-def coord_to_bitmap(coord):
-    return 1 << (24-coord_to_num(coord))
-
-def bitmap_to_coords(bitmap):
-    for i in range(24, -1, -1):
-        if bitmap & 1:
-            yield num_to_coord(i)
-        bitmap >>= 1
-
-def coords_to_integer(coords, offset=lambda x:x):
-    number = encode_player(offset(coords[0]))
-    number = (number << 5) + encode_player(offset(coords[1]))
-    number <<= 25
-    for coord in coords[2:]:
-        number |= coord_to_bitmap(offset(coord))
-    return number
-
-def integer_to_coords(number, offset=lambda x:x):
-    coords = [offset(b) for b in bitmap_to_coords(number)]
-    number >>= 25
-    player2 = offset(decode_player(number))
-    number >>= 5
-    player1 = offset(decode_player(number))
-
-    return [player1, player2] + sorted(coords)
-
+from board_encoding import create_encoding_class, no_offset
 
 debug = 1
 if __name__ == '__main__':
+
+    dim = 5
+    enc = create_encoding_class(5)()
 
     for r in range(5):
         for c in range(6):
@@ -73,8 +14,8 @@ if __name__ == '__main__':
                 coord = None
             else:
                 coord = (r, c)
-            num = encode_player(coord)
-            check = decode_player(num)
+            num = enc.encode_player(coord)
+            check = enc.decode_player(num)
             match = coord == check
             if debug or not match:
                 print(str(num).rjust(8), bin(num)[2:].zfill(5), coord, check, coord==check)
@@ -84,22 +25,22 @@ if __name__ == '__main__':
     for r in range(5):
         for c in range(5):
             coord = (r, c)
-            num = coord_to_bitmap(coord)
-            check = [b for b in bitmap_to_coords(num)]
+            num = enc.coord_to_bitmap(coord)
+            check = [b for b in enc.bitmap_to_coords(num)]
             match = len(check)==1 and check[0] == coord
             if debug or not match:
-                print(str(num).rjust(8), format_bitmap(num), coord, check[0], coord==check[0])
+                print(str(num).rjust(8), enc.format_bitmap(num), coord, check[0], coord==check[0])
             assert match
 
     if debug: print()
     enc_test = [(3, 2), None, (1, 2), (2, 4), (0, 1), (3, 0), (0, 0), (4, 2)]
 
     enc_test = enc_test[:2] + sorted(enc_test[2:])
-    num = coords_to_integer(enc_test)
-    check = integer_to_coords(num)
+    num = enc.coords_to_integer(enc_test, no_offset)
+    check = enc.integer_to_coords(num, no_offset)
     match = check == enc_test
     if debug or not match:
-        print(str(num).rjust(11), format_bitmap(num))
+        print(str(num).rjust(11), enc.format_bitmap(num))
         print(check)
         print(enc_test)
     assert match
@@ -111,27 +52,27 @@ if __name__ == '__main__':
                               ( 1, -2), ( 1, -1), ( 1, 0), ( 1, 1), ( 1, 2),
                               ( 2, -2), ( 2, -1), ( 2, 0), ( 2, 1), ( 2, 2)]
     all_ones = (2**25)-1
-    check = integer_to_coords(all_ones, up_left)
+    check = enc.integer_to_coords(all_ones) # Using default up_left offset
     match = check == all_coords
     if debug or not match:
-        print(str(all_ones).rjust(11), format_bitmap(all_ones))
+        print(str(all_ones).rjust(11), enc.format_bitmap(all_ones))
         print(check)
     assert match
 
     if debug: print()
-    check = coords_to_integer(all_coords, down_rt)
+    check = enc.coords_to_integer(all_coords) # Using default down_rt offset
     match = check == all_ones
     if debug or not match:
-        print(str(all_ones).rjust(11), format_bitmap(all_ones))
-        print(str(check).rjust(11), format_bitmap(check))
+        print(str(all_ones).rjust(11), enc.format_bitmap(all_ones))
+        print(str(check).rjust(11), enc.format_bitmap(check))
     assert match
 
     if debug: print()
     players = [None, (0, -1)]
     bitmap = ((2*5+1)+1)<<25
-    check = integer_to_coords(bitmap, up_left)
+    check = enc.integer_to_coords(bitmap)#, up_left)
     match = check == players
     if debug or not match:
-        print(str(bitmap).rjust(11), format_bitmap(bitmap))
+        print(str(bitmap).rjust(11), enc.format_bitmap(bitmap))
         print(check)
     assert match
