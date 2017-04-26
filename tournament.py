b95d367258de07b4b1260d9713bdcc18eb9b8076
Richard Milne <richmilne@hotmail.com>
2017-04-26 10:09:23 +0200

    Record, and display, the number of matches lost because of timeouts.

--- tournament.py
+++ tournament.py
@@ -99,7 +99,8 @@ def play_match(player1, player2):
     if sum(num_timeouts.values()) != 0:
         warnings.warn(TIMEOUT_WARNING)
 
-    return num_wins[player1], num_wins[player2]
+    return (num_wins[player1], num_timeouts[player1],
+            num_wins[player2], num_timeouts[player2])
 
 
 def play_round(agents, num_matches):
@@ -116,21 +117,27 @@ def play_round(agents, num_matches):
     for idx, agent_2 in enumerate(agents[:-1]):
 
         counts = {agent_1.player: 0., agent_2.player: 0.}
+        touts  = {agent_1.player: 0., agent_2.player: 0.}
         names = [agent_1.name, agent_2.name]
-        print("  Match {}: {!s:^11} vs {!s:^11}".format(idx + 1, *names), end=' ')
+        print("  Match {}: {!s:^18} vs {!s:^18}".format(idx + 1, *names), end=' ')
 
         # Each player takes a turn going first
         for p1, p2 in itertools.permutations((agent_1.player, agent_2.player)):
             for _ in range(num_matches):
-                score_1, score_2 = play_match(p1, p2)
+                score_1, tout_1, score_2, tout_2 = play_match(p1, p2)
                 counts[p1] += score_1
                 counts[p2] += score_2
+                touts[p1] += tout_1
+                touts[p2] += tout_2
                 total += score_1 + score_2
 
         wins += counts[agent_1.player]
 
-        print("\tResult: {} to {}".format(int(counts[agent_1.player]),
-                                          int(counts[agent_2.player])))
+        print("\tResult: {}/{} to {}/{}".format(int(counts[agent_1.player]),
+                                                int(touts[agent_1.player]),
+                                                int(counts[agent_2.player]),
+                                                int(touts[agent_2.player])
+                                                ))
 
     return 100. * wins / total
 
@@ -144,15 +151,20 @@ def main():
     MM_ARGS = {"search_depth": 3, "method": 'minimax', "iterative": False}
     CUSTOM_ARGS = {"method": 'alphabeta', 'iterative': True}
 
+    def create_agent(args, heuristic):
+        prefix = 'MM' if args['method'] == 'minimax' else 'AB'
+        depth = 'inf' if args['iterative'] else str(args['search_depth'])
+        name = '%s_%s_%s' % (prefix, heuristic[0], depth)
+        player = CustomPlayer(score_fn=heuristic[1], **args)
+        return Agent(player, name)
+
     # Create a collection of CPU agents using fixed-depth minimax or alpha beta
     # search, or random selection.  The agent names encode the search method
     # (MM=minimax, AB=alpha-beta) and the heuristic function (Null=null_score,
     # Open=open_move_score, Improved=improved_score). For example, MM_Open is
     # an agent using minimax search with the open moves heuristic.
-    mm_agents = [Agent(CustomPlayer(score_fn=h, **MM_ARGS),
-                       "MM_" + name) for name, h in HEURISTICS]
-    ab_agents = [Agent(CustomPlayer(score_fn=h, **AB_ARGS),
-                       "AB_" + name) for name, h in HEURISTICS]
+    mm_agents = [create_agent(MM_ARGS, h) for h in HEURISTICS]
+    ab_agents = [create_agent(AB_ARGS, h) for h in HEURISTICS]
     random_agents = [Agent(RandomPlayer(), "Random")]
 
     # ID_Improved agent is used for comparison to the performance of the
@@ -160,8 +172,9 @@ def main():
     # systems; i.e., the performance of the student agent is considered
     # relative to the performance of the ID_Improved agent to account for
     # faster or slower computers.
-    test_agents = [Agent(CustomPlayer(score_fn=improved_score, **CUSTOM_ARGS), "ID_Improved"),
-                   Agent(CustomPlayer(score_fn=custom_score, **CUSTOM_ARGS), "Student")]
+    test_heuristics = [('ID_Improved', improved_score),
+                       ('Student', custom_score)]
+    test_agents = [create_agent(CUSTOM_ARGS, h) for h in test_heuristics]
 
     print(DESCRIPTION)
     for agentUT in test_agents:
@@ -175,7 +188,7 @@ def main():
 
         print("\n\nResults:")
         print("----------")
-        print("{!s:<15}{:>10.2f}%".format(agentUT.name, win_ratio))
+        print("{!s:<18}{:>10.2f}%".format(agentUT.name, win_ratio))
 
 
 if __name__ == "__main__":
